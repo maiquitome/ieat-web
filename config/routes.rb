ab43957fffb8a2c9f4f7d5054c3301d3e8efcc32
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-07-13 14:07:35 -0300

    :sparkles: add products views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  # get 'products/index'
+  resources :products, only: [:index, :new, :create]
+
   resources :restaurants
+
   devise_for :users
 
   root 'home#index'
